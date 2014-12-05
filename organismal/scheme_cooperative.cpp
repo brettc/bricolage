b248d82a71f5f3818d1b40ce2a0d6b67647b7970
Brett Calcott <brett.calcott@gmail.com>
2014-12-04 17:06:15 -0700

    Update new Factory

--- organismal/scheme_cooperative.cpp
+++ organismal/scheme_cooperative.cpp
@@ -4,16 +4,20 @@ using namespace pubsub2;
 
 cSchemeCoop3::cSchemeCoop3(cFactory *f, double rate_per_gene_)
     : cGeneFactory(f, rate_per_gene_)
+    , r_binding(-3, 3)
 {
 }
 
 cCisModule *cSchemeCoop3::construct_cis()
 {
     cCisModuleCoop3 *cis = new cCisModuleCoop3();
-    // random_engine_t &re = factory->random_engine;
-    // cis->op = factory->operands[r_oper(re)];
-    // cis->channels[0] = r_sub(re);
-    // cis->channels[1] = r_sub(re);
+    random_engine_t &re = factory->random_engine;
+
+    for (size_t i = 0; i < cis->site_count(); ++i)
+    {
+        cis->channels[i] = r_sub(re);
+        cis->binding[i] = r_binding(re);
+    }
     return cis;
 }
 
@@ -26,13 +30,23 @@ void cSchemeCoop3::mutate_cis(cCisModule *m)
 cCisModule *cCisModuleCoop3::clone() const
 {
     cCisModuleCoop3 *cis = new cCisModuleCoop3();
-    // cis->op = op;
-    // cis->channels[0] = channels[0];
-    // cis->channels[1] = channels[1];
+    for (size_t i = 0; i < cis->site_count(); ++i)
+    {
+        cis->channels[i] = channels[i];
+        cis->binding[i] = binding[i];
+    }
     return cis;
 }
 
 bool cCisModuleCoop3::is_active(cChannelState const &state) const 
 {
-    return true;
+    // Calculate the weighted sum
+    int_t sum = 0;
+    for (size_t i = 0; i < site_count(); ++i)
+    {
+        if (state[channels[i]])
+            sum += binding[i];
+    }
+    // Thresholded
+    return sum >= 3;
 }
--- organismal/scheme_cooperative.h
+++ organismal/scheme_cooperative.h
@@ -6,6 +6,9 @@ struct cSchemeCoop3 : public cGeneFactory
 {
     cSchemeCoop3(cFactory *f, double rate_per_gene_);
 
+    // Randomize binding
+    randint_t r_binding;
+
     // virtual overrides
     cCisModule *construct_cis();
     void mutate_cis(cCisModule *m);
@@ -22,6 +25,7 @@ public:
     void mutate();
     bool is_active(cChannelState const &state) const;
 
+    int_t binding[3];
     signal_t channels[3];
 };
 
