42e045a723d74c6b7a3415603da68fc481942caf
Brett Calcott <brett.calcott@gmail.com>
2014-12-17 23:13:46 -0700

    Analyse bindingg scheme

--- extra/davidson_binding.py
+++ extra/davidson_binding.py
@@ -1,35 +1,41 @@
 """
-A Quick test of how to convert cooperative binding strengths into boolean
+An explorating of how to convert cooperative binding strengths into boolean
 operators. This generates (roughly) the kind of boolean operators that we see
-in the Davidson-Peters models. IE. No crazy NAND, XOR or anything.
+in the Davidson-Peters models.
 """
 
-
 import itertools
-from sympy.logic import SOPform #, POSform
-the_form = SOPform
+from sympy.logic import SOPform, simplify_logic
 from sympy.printing.latex import latex
 
-def detex(term):
+def term_to_simple_text(term):
+    """This appears to be the easiest way to write the functions in a simple manner"""
     rt = latex(term)
     rt = rt.replace(r"\left", "")
     rt = rt.replace(r"\right", "")
     rt = rt.replace(r"\wedge", "&")
     rt = rt.replace(r"\vee", "|")
     rt = rt.replace(r"\neg ", "~")
+    rt = rt.replace(r"\mathrm{", "")
+    rt = rt.replace(r"}", "")
+    rt = rt.replace(" ", "")
     rt = rt.replace(" ", "")
     return rt
 
-def make_bindings(size, thresh_adjust=0):
+def make_bindings(size, on_channel=False, thresh_adjust=0):
     """
     The rough idea. Each "site" either contributes to or detracts from the
     binding. Assuming there are three sites, We assign each site from -3 to +3.
     If a site is "bound", then we add the amount at that site. 
     """
-
+    # Not sure this should be size or input size
     strengths = range(-size, size+1)
     # Assign each site a..z
-    letters = [chr(ord('a') + i) for i in range(size)]
+
+    input_size = size
+    if on_channel:
+        input_size += 1
+    letters = [chr(ord('a') + i) for i in range(input_size)]
 
     soplist = {}
     threshold = size - thresh_adjust
@@ -39,36 +45,94 @@ def make_bindings(size, thresh_adjust=0):
     inputs = list(itertools.product((0, 1), repeat=size))
 
     # This generates all possible combinations of binding strengths.
-    for bindings in itertools.product(strengths, repeat=size):
+    for bindings in itertools.product(strengths, repeat=input_size):
         minterms = []
 
         # Iterate through all inputs for this
         for i in inputs:
-            active = [a for a, b in zip(bindings, i) if b == 1]
+            # If there is an on_channel (a background channel always on), then
+            # we add this as channel 0
+            if on_channel:
+                cur_input = [1] + list(i)
+            else:
+                cur_input = i
+            active = [a for a, b in zip(bindings, cur_input) if b == 1]
             if sum(active) >= threshold:
                 minterms.append(i)
 
         # Now, what boolean function does that set of combinations work for?
-        sop = the_form(letters, minterms)
+        sop = SOPform(letters, minterms)
 
-        if len(sop.atoms()) > 0:
+        # if len(sop.atoms()) > 0:
             # Some of them are repeated
-            b = soplist.setdefault(sop, [])
-            b.append(bindings)
+        b = soplist.setdefault(sop, [])
+        b.append(bindings)
 
     return soplist
 
-sops = make_bindings(3)
+def print_sops(sops, count=False):
+    # Look at the distribution of boolean functions that are generated. Some occur
+    # more than others ... 
+    print "============="
+    print "There are {} items!".format(len(sops))
+    print "------------"
+    if count:
+        sort_sops = []
+        for k, v in sops.iteritems():
+            sort_sops.append((sum(v), k))
+    else:
+        sort_sops = [(len(v), k) for k, v in sops.items()]
+    total = 0
+    for number, eq in sorted(sort_sops, key=lambda x: x[0]):
+        # print term_to_simple_text(eq), number
+        print "${}$ | {}".format(latex(eq), number)
+        total += number
+    print "------------"
+    print "A total of {} combinations".format(total)
+    print "============="
 
-# Look at the distribution of boolean functions that are generated. Some occur
-# more than others ... We might want to change these via probabilities..?
-sort_sops = [(len(v), k) for k, v in sops.items()]
-for p in sorted(sort_sops, key=lambda x: x[0]):
-    print p
+def run_test2():
+    size = 2
+    sops = make_bindings(size)
+    print '----normal'
+    print_sops(sops)
+    sops_c = make_bindings(size, on_channel=True, thresh_adjust=0)
+    print '----constitutive'
+    print_sops(sops_c)
+
+def run_two_modules():
+    double_soplist = {}
+    sops = make_bindings(3, on_channel=True, thresh_adjust=0)
+    keys = sops.keys()
+    # Use the cross-product to generate all combinations
+    for m1, m2 in itertools.product(keys, keys):
+        # Give us DNF form
+        both = simplify_logic(m1 | m2, 'dnf')
+        # Some of them are repeated
+        b = double_soplist.setdefault(both, [])
+        # Don't append the keys, append the amounts
+        total = len(sops[m1]) * len(sops[m2])
+        b.append((total))
+
+    print_sops(double_soplist, count=True)
+
+    # print "There are {} items!".format(len(double_soplist))
+    # for sop, pairs in double_soplist.items():
+    #     print term_to_simple_text(sop), len(pairs)
+    # prin
+    # one = sops.keys()[0]
+    # two = sops.keys()[1]
+    # print one, two
+    # print one | two
+
+if __name__ == '__main__':
+    # run_test2()
+    run_two_modules()
+
+
 
 
 # Mutation idea ---
-#
 # Mutations add or subtract strengths. If they go through 0, they can turn
 # into another kind of binding.. That is cute.
 
