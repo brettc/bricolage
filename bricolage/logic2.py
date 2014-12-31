14203c51f9402bdc23220b327f95f46c6d943e03
Brett Calcott <brett.calcott@gmail.com>
2014-12-31 17:27:11 -0500

    Missing imports added

--- bricolage/logic2.py
+++ bricolage/logic2.py
@@ -1,14 +1,14 @@
 from .operand import Operand
 import core
-from core_ext import World, Network, NetworkCollection
+from core_ext import World, Network, NetworkCollection, Target, NetworkAnalysis
 from logic2_ext import Constructor
 
 __all__ = ["World", "Constructor", "Network", "Parameters", "Operand",
-           "NetworkCollection"]
+           "NetworkCollection", "Target", "NetworkAnalysis"]
 
 class Parameters(core.Parameters):
     def __init__(self, **kwargs):
         core.Parameters.__init__(self, **kwargs)
-        self.operands = [Operand.AND, Operand.OR, Operand.A_AND_NOT_B]
+        self.operands = [Operand.AND, Operand.OR]
         self._override(kwargs)
 
