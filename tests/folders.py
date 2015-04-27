038afb6a8d779714628d5b9083ef883bc8085a5d
Brett Calcott <brett.calcott@gmail.com>
2015-04-27 15:01:03 -0700

    Add a generate script to the tests to allow for complex persistent data

---
+++ tests/folders.py
@@ -0,0 +1,8 @@
+import pathlib
+import sys
+
+# Add the parent here
+here_dir = pathlib.Path(__file__).parent.absolute()
+parent_dir = here_dir.parent
+data_dir = here_dir / 'data'
+sys.path.append(str(parent_dir))
