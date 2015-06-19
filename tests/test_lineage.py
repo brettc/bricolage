ea2ac975da4db31f007685177456e69d0463afc0
Brett Calcott <brett.calcott@gmail.com>
2015-06-19 16:30:17 -0700

    New experiment format devised -- testing still ongoing

--- tests/test_lineage.py
+++ tests/test_lineage.py
@@ -271,32 +271,3 @@ def test_targets(p_3x2, tmpdir):
         fb1 = [n.fitness for n in b.get_generation(pa1_gen)]
         assert fa1 == fb1
 
-def test_treatment(tmpdir, p_3x2):
-    tmpdir = pathlib.Path(str(tmpdir))
-    name = 'treat'
-    path = tmpdir / name
-    apath = tmpdir / (name + '_analysis')
-    treat = L.Treatment(path, p_3x2, analysis_path=apath)
-    max_gen = 100
-
-    for rep in treat.iter_replicates():
-        with rep.get_lineage() as lin:
-            lin.add_target(target1)
-            while lin.generation < max_gen:
-                lin.next_generation()
-
-        assert rep.analysis_path is not rep.path
-        assert rep.analysis_path.exists()
-
-    with treat.replicates[5].get_lineage() as l5:
-        assert l5.population
-    
-    # Kill this
-    del treat
-
-    treat = L.Treatment(path, p_3x2)
-    for rep in treat.iter_replicates():
-        with rep.get_lineage() as lin:
-            assert lin.generation == max_gen
-
-
