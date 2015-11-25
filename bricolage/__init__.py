c695c918c9c081ea3abf1502ed2fe65dd83988a9
Brett Calcott <brett.calcott@gmail.com>
2015-11-26 11:06:30 +1300

    Display the correct License.

--- bricolage/__init__.py
+++ bricolage/__init__.py
@@ -8,5 +8,5 @@ __uri__ = "https://github/brettc/bricolage/"
 __author__ = "Brett Calcott"
 __email__ = "brett.calcott@gmail.com"
 
-__license__ = "MIT"
+__license__ = "GPL3"
 __copyright__ = "Copyright (c) 2015 Brett Calcott"
