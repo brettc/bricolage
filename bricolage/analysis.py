Add fixes for average control measure.

bricolage/analysis.py
@@ -4,7 +4,9 @@
 import numpy as np
 import pandas as pd
 
-from .analysis_ext import MutualInfoAnalyzer, CausalFlowAnalyzer, Information, NetworkAnalysis
+from .analysis_ext import (
+    MutualInfoAnalyzer, CausalFlowAnalyzer, AverageControlAnalyzer,
+    Information, NetworkAnalysis)
 from .lineage import FullLineage
 from .graph import SignalFlowGraph
 from neighbourhood import NetworkNeighbourhood, PopulationNeighbourhood
@@ -15,6 +17,7 @@ class InfoSummarizer(object):
         assert isinstance(lin, FullLineage)
         self._lineage = lin
         self._cf = CausalFlowAnalyzer(lin.world, flow)
+        self._af = AverageControlAnalyzer(lin.world, flow)
         self._mf = MutualInfoAnalyzer(lin.world, target.calc_categories())
         self._fits = np.zeros(lin.params.population_size)
         self.params = lin.params
@@ -25,36 +28,47 @@ class InfoSummarizer(object):
         names = []
         for c in range(regs):
             names.append('C_{}'.format(c+1))
+            names.append('A_{}'.format(c+1))
         names.extend('C_MEAN C_MAX C_MXMN'.split())
-        names.extend('FIT_MN FIT_VA FIT_MX'.split())
+        names.extend('F_MEAN F_VAR F_MAX'.split())
+        names.extend('A_MEAN A_MAX A_MXMN'.split())
         return names
 
     def get_values(self, g):
         ci = self._cf.numpy_info_from_collection(g)
+        ai = np.asarray(self._af.analyse_collection(g))
         g.get_fitnesses(self._fits)
 
         # Sum the information across the outputs
         csummed = ci.sum(axis=2)
+        asummed = ai.sum(axis=2)
 
         # Get means across the population
         cmeans = np.mean(csummed, axis=0)
+        ameans = np.mean(asummed, axis=0)
 
         vals = []
         regs = self.params.reg_channels
         for i, c in enumerate(range(regs)):
             vals.append(('C_{}'.format(c+1), cmeans[i]))
+            vals.append(('A_{}'.format(c+1), ameans[i]))
 
         # Now get the whole sum?
         ctot = csummed.sum(axis=1)
+        atot = asummed.sum(axis=1)
         vals.extend([
             ('C_MEAN', ctot.mean()),
             ('C_MAX', csummed.max()),
             ('C_MXMN', cmeans.max()),
+            ('A_MEAN', atot.mean()),
+            ('A_MAX', asummed.max()),
+            ('A_MXMN', ameans.max()),
             ('F_MEAN', self._fits.mean()),
             ('F_VAR', self._fits.var()),
             ('F_MAX', self._fits.max()),
         ])
         return vals
+
 
 class NeighbourhoodSummarizer(object):
     def __init__(self, lin, target):
@@ -94,6 +108,13 @@ def make_population_frames(pop, target, flow, do_cuts=True):
     cframe = pd.DataFrame(csummed)
     cframe.columns = ["C{}".format(i) for i in range(1, len(cframe.columns) + 1)]
 
+    # Control analysis
+    af = AverageControlAnalyzer(pop.factory.world, flow)
+    ai = np.asarray(af.analyse_collection(pop))
+    asummed = ai.sum(axis=2)
+    aframe = pd.DataFrame(asummed)
+    aframe.columns = ["A{}".format(i) for i in range(1, len(aframe.columns) + 1)]
+
     mf = MutualInfoAnalyzer(pop.factory.world, target.calc_categories())
     mi = mf.numpy_info_from_collection(pop)
     msummed = mi.sum(axis=2)
@@ -102,7 +123,7 @@ def make_population_frames(pop, target, flow, do_cuts=True):
 
     # Cuts are time-consuming...
     if not do_cuts:
-        return fits_frame, mframe, cframe
+        return fits_frame, mframe, cframe, aframe
 
     cuts = []
     for n in pop:
@@ -111,15 +132,12 @@ def make_population_frames(pop, target, flow, do_cuts=True):
         cuts.append(len(fg.minimum_cut()))
     cuts_frame = pd.DataFrame({'cuts': cuts})
 
-    return fits_frame, cuts_frame, mframe, cframe
+    return fits_frame, cuts_frame, mframe, cframe, aframe
 
 
 def make_population_frame(pop, target, flow, do_cuts=True):
     return pd.concat(make_population_frames(pop, target, flow, do_cuts), axis=1)
 
-
-# def make_summary_record(pop, target, flow)
-#
 
 def make_ancestry_frames(anc, target, flow):
     target.assess_collection(anc)