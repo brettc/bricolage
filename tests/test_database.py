b56a8b4a43ed0e584a7a1c999604235bdbae291f
Brett Calcott <brett.calcott@gmail.com>
2016-02-05 15:14:34 +1300

    Fix some previous fooling with tests (remove database)

--- tests/test_database.py
+++ tests/test_database.py
@@ -4,16 +4,16 @@ from bricolage.stats import (StatsAverageControl, StatsFitness,
                              StatsOutputControl)
 
 
-def test_database(simple_experiment):
-    assert isinstance(simple_experiment, Experiment)
-
-    exp = simple_experiment
-    exp.database.create(True)
-
-    visitor = StatsVisitor(exp, [StatsMutualInformation])
-    exp.visit_generations(visitor, every=10)
+# def test_database(simple_experiment):
+#     assert isinstance(simple_experiment, Experiment)
+#
+#     exp = simple_experiment
+#     exp.database.create(True)
+#
+#     visitor = StatsVisitor(exp, [StatsMutualInformation])
+#     exp.visit_generations(visitor, every=10)
 
     # This reloads everything, so it will exclude what has been saved.
     # visitor = StatsVisitor(exp, [StatsAverageControl, StatsFitness])
-    visitor = StatsVisitor(exp, [StatsOutputControl])
-    exp.visit_generations(visitor, every=10)
+    # visitor = StatsVisitor(exp, [StatsOutputControl])
+    # exp.visit_generations(visitor, every=10)
