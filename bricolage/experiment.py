336cc5d99a3d7a29b65e1eaaa64a46df61cb5201
Brett Calcott <brett.calcott@gmail.com>
2016-02-15 14:35:51 +1300

    Correct network printing bug

--- bricolage/experiment.py
+++ bricolage/experiment.py
@@ -191,9 +191,8 @@ class Replicate(object):
                  knockouts=True,
                  target=None):
         ana = NetworkAnalysis(net)
-        ana.calc_output_control()
-        if target:
-            ana.calc_mutual_info(target)
+        print target.as_array()
+        ana.annotate(target)
         g = get_graph_by_type(graph_type, ana)
         d = DotMaker(g)
         p = self.analysis_path / '{}-G{:07d}-N{:02d}-F{}.png'.format(
