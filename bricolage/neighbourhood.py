0724f0ea0addb94c4df9ca6012e194534a6c2db2
Brett Calcott <brett.calcott@gmail.com>
2017-03-22 12:44:40 +1300

    Clean up comments

--- bricolage/neighbourhood.py
+++ bricolage/neighbourhood.py
@@ -11,14 +11,17 @@ class NetworkNeighbourhood(object):
     that the expected number of single-step walks is proportion of one-step
     neighbourhood.
     """
-    def __init__(self, center, sample_size, one_step_proportion=1.0, lamb=0.0):
-        # Sample from the neighbourhood
-        #
-        # @center: our focal network.
-        # @one_step_proportion: the proportion of the sample that we want to
-        # have only one step mutations
-        #
-        # self.one_step_proportion = one_step_proportion
+    def __init__(self, center, sample_size, 
+                 one_step_proportion=1.0, 
+                 lamb=0.0,
+                 steps=1.0,
+                 ):
+        """There three ways to generation a neighbourhood.
+            - specify the number of steps
+            - specifiy the proportion that will be one step.
+            - specify a lambda parameter / mean for sampling usig poisson.
+
+        """
         self.sample_size = sample_size
         self.center = center
 
@@ -32,7 +35,9 @@ class NetworkNeighbourhood(object):
             # We want at least one mutation, so add one.
             self.mutations = np.random.poisson(self.lamb, self.sample_size) + 1
         else:
-            self.mutations = np.ones(self.sample_size, dtype=int)
+            self.mutations = np.ones(self.sample_size, dtype=int) * steps
+            # print self.mutations[:10]
+
 
         self.neighbours = Collection(center.factory)
         self.neighbours.fill_with_mutations(center, self.mutations)
@@ -40,7 +45,13 @@ class NetworkNeighbourhood(object):
 
 class PopulationNeighbourhood(object):
     """The same thing, for populations"""
-    def __init__(self, population, sample_size, one_step_proportion=1.0, lamb=0.0):
+    def __init__(self, population, sample_size, 
+                 one_step_proportion=1.0, 
+                 lamb=0.0,
+                 steps=1.0,
+                 ):
+        """See above"""
+
         self.one_step_proportion = one_step_proportion
         self.sample_size = sample_size
         self.population = population
@@ -58,7 +69,7 @@ class PopulationNeighbourhood(object):
             if lamb > 0.0:
                 mutes = np.random.poisson(self.lamb, self.sample_size) + 1
             else:
-                mutes = np.ones(self.sample_size, dtype=int)
+                mutes = np.ones(self.sample_size, dtype=int) * steps
 
             network_neighbours = Collection(population.factory)
             network_neighbours.fill_with_mutations(n, mutes)
